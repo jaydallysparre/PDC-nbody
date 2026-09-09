bf4680f6008c7add65086e951b402cce47165645
jaydallysparre <a1950230@adelaide.edu.au>
2026-09-09 09:10:00 +0000

    Added bash script for running test cases

---
+++ comp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+mkdir .temp
+make all DIR=.temp
+
+# test for a number of different process counts
+procN=(1 2 3 4 6 12)
+
+for test in testcases/*.test; do
+    name=$(basename "$test" .test)
+
+    for n in "${procN[@]}"; do
+        echo "-----------------------------------------------------------------"
+        echo "TEST INPUT: $test ON $n PROCESSES"
+        echo "-----------------------------------------------------------------"
+        mpirun -n "$n" .temp/nbodyref $(cat "$test") < testcases/12_particle.ic > ".temp/${name}_${n}_ref"
+        mpirun -n "$n" .temp/nbody1a $(cat "$test") < testcases/12_particle.ic > ".temp/${name}_${n}_1a"
+
+        # diff check with last line (timing) removed
+        diff <(sed '$d' ".temp/${name}_${n}_ref") <(sed '$d' ".temp/${name}_${n}_1a") > /dev/null
+        statusA=$? # exit status for ref vs 1a
+
+        if [ "$statusA" -eq 0 ]; then
+            echo "A PASS"
+        else
+            echo "A FAILED"
+        fi
+
+        # placed after because its easier to catch mpi errors
+        mpirun -n "$n" .temp/nbody1b $(cat "$test") < testcases/12_particle.ic > ".temp/${name}_${n}_1b"
+
+        diff <(sed '$d' ".temp/${name}_${n}_ref") <(sed '$d' ".temp/${name}_${n}_1b") > /dev/null
+        statusB=$? # exit status for ref vs 1b
+
+        if [ "$statusB" -eq 0 ]; then
+            echo "B PASS"
+        else
+            echo "B FAILED"
+        fi
+    done
+done
+
+#rm -rf .temp
+
--- makefile
+++ makefile
@@ -1,16 +1,19 @@
 EXECS=nbody1a nbody1b nbodyref
 MPICC?=mpicc
 
+# going to redirect for tests
+DIR ?= .
+
 all: ${EXECS}
 
-mpi_part1a: part1a.c
-	${MPICC} -o nbody1a part1a.c -lm
+nbody1a: part1a.c
+	${MPICC} -o ${DIR}/nbody1a part1a.c -lm
 
-mpi_part1b: part1b.c
-	${MPICC} -o nbody1b part1b.c -lm
+nbody1b: part1b.c
+	${MPICC} -o ${DIR}/nbody1b part1b.c -lm
 
-reference: mpi_nbody_basic.c
-	${MPICC} -o nbodyref mpi_nbody_basic.c -lm
+nbodyref: mpi_nbody_basic.c
+	${MPICC} -o ${DIR}/nbodyref mpi_nbody_basic.c -lm
 
 clean:
 	rm -f ${EXECS}
